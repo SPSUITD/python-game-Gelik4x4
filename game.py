30cb074a49bf5cbfe428cfb290a3bda37e9a7462
Gelik <113053733+Gelik4x4@users.noreply.github.com>
2024-03-05 21:43:48 +0300

    Created main file with background and running cat. Added requirements. Created cat, game, files for future work.

---
+++ game.py
@@ -0,0 +1,8 @@
+import pygame
+
+SIZE = WIDTH, HEIGHT = 1200, 600
+SCREEN = pygame.display.set_mode(SIZE)
+FPS = 30
+TILE_SIZE = 40
+
+
--- main.py
+++ main.py
@@ -1,1 +1,38 @@
-print("Hello, world!")
+import pygame
+SIZE = WIDTH, HEIGHT = 1200, 600
+SCREEN = pygame.display.set_mode(SIZE)
+TILE_SIZE = 40
+FPS = 30
+icon = pygame.image.load('images/icon.png')
+pygame.display.set_icon(icon)
+pygame.display.set_caption('CAT-LIFE')
+bg_image = pygame.image.load('images/background.png')
+
+def main():
+    walk_right = [
+        pygame.image.load('sprites/right_1.png'),
+        pygame.image.load('sprites/right_2.png'),
+        pygame.image.load('sprites/right_3.png'),
+        pygame.image.load('sprites/right_4.png'),
+        pygame.image.load('sprites/right_5.png'),
+        pygame.image.load('sprites/right_6.png'),
+    ]
+    an_count = 0
+    pygame.init()
+    game = True
+    while game:
+        SCREEN.blit(bg_image, (0, 0))
+        SCREEN.blit(walk_right[an_count], (300, 300))
+        if an_count == 5:
+            an_count = 0
+        else:
+            an_count += 1
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game = False
+                pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
